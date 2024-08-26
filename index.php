7fd0993cd51c67e8ae88ccf13d77cfc7d635588f
Sebastian Deseke <deseke97@gmail.com>
2024-08-26 15:14:16 +0200

    Testing button redirecting

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php include "includes/header.php";
+ require __DIR__ . '/vendor/autoload.php';
+
+
 
 $env = file_get_contents(__DIR__ . '/.env');
 // $dotenv = Dotenv\Dotenv::createUnsafeUnsafeImmutable(__DIR__);
@@ -7,5 +10,10 @@ $env = file_get_contents(__DIR__ . '/.env');
 
 <h2>Welcome to the Rugby Portal</h2>
 <p>Use this portal to manage players, penalties, and discussions.</p>
+
+<button type="button" class="btn btn-primary" onclick="window.location.href='/players'">Players</button>
+<button type="button" class="btn btn-primary" onclick="window.location.href='/penalties'">Penalties</button>
+<button type="button" class="btn btn-primary" onclick="window.location.href='/roles'">Roles</button>
+<button type="button" class="btn btn-primary" onclick="window.location.href='/discussions'">Discussions</button>
 
 <?php include "includes/footer.php"; ?>
---
+++ pages/players.php
@@ -0,0 +1,9 @@
+<?php
+include '../includes/header.php';
+include '../includes/db.php';
+
+?>
+
+<h1>This page will display the players, i think</h1>
+
+<?php include '../includes/footer.php'; ?>
